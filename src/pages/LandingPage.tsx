df795ebce7999a3812836d12629e80c0c895ea02
junerey <junereyilajas-it@srcb.edu.ph>
2026-05-28 11:20:21 +0800

    sixth commit: added new files and updated MassBook system features

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -34,13 +34,7 @@ function LandingPage() {
     <div className="landing-shell">
       <header className="landing-header">
         <div className="landing-brand">
-          <img
-            className="brand-icon"
-            src="https://scontent.fceb1-3.fna.fbcdn.net/v/t39.30808-6/276156596_125507780061132_6269600553415109799_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG9UfAzafALCboy4uwHQtt-wkSZm6otrfDCRJmbqi2t8JAgK9g3KbqdCvjQAoTZGu54LGxP_bHXtzSr8mWvor4f&_nc_ohc=M_EcETu3uqwQ7kNvwHx6mrk&_nc_oc=Adq_mBBCGDExTXhMELdbE7bvtStR4LMVVyBDZ_OWWxDd-_vvJ236s7MxYojFouteO9o&_nc_zt=23&_nc_ht=scontent.fceb1-3.fna&_nc_gid=oGxunlUH92DonYK4rz_mpQ&_nc_ss=7b2a8&oh=00_Af6bBtUZV1tShdGUh1riUUwIC2Wt5DRFG4XwaY19VAwFEw&oe=6A1458DC"
-            alt="MassBook Logo"
-            style={{ objectFit: 'cover' }}
-            referrerPolicy="no-referrer"
-          />
+          <span className="brand-icon brand-cross" aria-hidden="true">✝</span>
           <div>
             <p className="small-label">MassBook</p>
             <strong>Parish Administration</strong>
